16bb1c99d5077a769fc7d5f70f0431854d6a4818
qyd <saviorbruce@gmail.com>
2019-08-30 17:16:30 -0700

    Updates

---
+++ precache-manifest.584edd19ed9b079f91fe61622462d39b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/forcebalance-manager/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "a85ca401da9e0e1c6586a40363cfdfb0",
+    "url": "/forcebalance-manager/static/media/ForceBalance.a85ca401.svg"
+  },
+  {
+    "revision": "97cf7eb14ad26d521389",
+    "url": "/forcebalance-manager/static/js/runtime~main.97cf7eb1.js"
+  },
+  {
+    "revision": "6a7f40a2c6c5cbf82349",
+    "url": "/forcebalance-manager/static/js/main.6a7f40a2.chunk.js"
+  },
+  {
+    "revision": "93d288a3fadd2f758f0e",
+    "url": "/forcebalance-manager/static/js/1.93d288a3.chunk.js"
+  },
+  {
+    "revision": "6a7f40a2c6c5cbf82349",
+    "url": "/forcebalance-manager/static/css/main.f86c2bf5.chunk.css"
+  },
+  {
+    "revision": "93d288a3fadd2f758f0e",
+    "url": "/forcebalance-manager/static/css/1.7cd2742f.chunk.css"
+  },
+  {
+    "revision": "479046505b192cc7c88bb55e7dcd31e4",
+    "url": "/forcebalance-manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/forcebalance-manager/precache-manifest.4f3f84d50b80773bb8ba748be7636171.js"
+  "/forcebalance-manager/precache-manifest.584edd19ed9b079f91fe61622462d39b.js"
 );
 
 workbox.clientsClaim();
